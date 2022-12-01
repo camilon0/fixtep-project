75494ec03f2fdb65c74c3d323fbcb210d60a5dd2
Ana-Rojas88 <anac.rojas88@gmail.com>
2022-12-01 15:35:02 -0500

    card

--- src/components/Contratistas.jsx
+++ src/components/Contratistas.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const Contratistas = () => {
   return (
@@ -8,7 +10,13 @@ const Contratistas = () => {
         <img src="./assets/fixtep.svg" alt="s" />
       </div>
       <div>Filtro</div>
-      <div>cartas</div>
+      <Card style={{ width: '11rem' }}>
+      <Card.Img variant="top" src="https://previews.123rf.com/images/dolgachov/dolgachov1610/dolgachov161012005/64860816-profesi%C3%B3n-carpinter%C3%ADa-ebanister%C3%ADa-y-el-concepto-de-la-gente-carpintero-con-tablones-de-madera-martil.jpg" />
+      <Card.Body>
+        <Card.Title>Carpintero</Card.Title>
+        <Button variant="primary">Detalles</Button>
+      </Card.Body>
+    </Card>
       <div>footer</div>
     </div>
   );
