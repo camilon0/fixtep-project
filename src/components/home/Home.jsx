106b4e536394d1079f53c17c718653ea4bc83475
camilon0 <kmilo8433@gmail.com>
2022-12-01 19:50:04 -0500

    nav and logo finished

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
+import logo from "./assets/logoConLetrasGrande.svg";
 import "./style.scss";
 
 const Home = () => {
   return (
     <div>
-      <div>logo con letra</div>
+      <div>
+        <img src={logo} alt="s" />
+      </div>
       <div>frase de la compania</div>
       <div>imagen backgraund</div>
       <div>dos botones: uno que redireccione a los contratistas </div>
