ea1e0dd20cc0969e395c0f676490dfee51574cbc
camilon0 <kmilo8433@gmail.com>
2022-12-01 21:31:12 -0500

    carousel

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,18 +1,76 @@
 import React from "react";
+import { Carousel } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
+//import { images } from "../../services/data";
+
 import logo from "./assets/logoConLetrasGrande.svg";
 import "./style.scss";
 
+const images = require.context("./assets/img", true);
+
 const Home = () => {
   return (
-    <div>
+    <>
+      <div>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="imgCarrusel d-block w-100"
+              src={images(`./img1.jpg`)}
+              alt="s"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="imgCarrusel d-block w-100"
+              src={images(`./img2.jpg`)}
+              alt="s"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="imgCarrusel d-block w-100"
+              src={images(`./img3.jpg`)}
+              alt="s"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="imgCarrusel d-block w-100"
+              src={images(`./img4.jpg`)}
+              alt="s"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="imgCarrusel d-block w-100"
+              src={images(`./img5.jpg`)}
+              alt="s"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="imgCarrusel d-block w-100"
+              src={images(`./img6.jpg`)}
+              alt="s"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="imgCarrusel d-block w-100"
+              src={images(`./img7.jpg`)}
+              alt="s"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </div>
       <div>
         <img src={logo} alt="s" />
       </div>
       <div>frase de la compania</div>
       <div>imagen backgraund</div>
       <div>dos botones: uno que redireccione a los contratistas </div>
-    </div>
+    </>
   );
 };
 
--- src/services/data.js
+++ src/services/data.js
@@ -1,18 +1,18 @@
 export const categoryWorker = [
-    {
-      label: "Carpintero",
-      value: 1,
-    },
-    {
-      label: "Albañil",
-      value: 2,
-    },
-    {
-      label: "Plomero",
-      value: 3,
-    },
-    {
-      label: "Electricista",
-      value: 4,
-    }
-  ];
+  {
+    label: "Carpintero",
+    value: 1,
+  },
+  {
+    label: "Albañil",
+    value: 2,
+  },
+  {
+    label: "Plomero",
+    value: 3,
+  },
+  {
+    label: "Electricista",
+    value: 4,
+  },
+];
