ce420d1d1348cd7510aa79f8c7f9fb451a8136c8
camilon0 <kmilo8433@gmail.com>
2022-12-02 14:10:23 -0500

    main home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -11,63 +11,71 @@ const images = require.context("./assets/img", true);
 const Home = () => {
   return (
     <>
-      <div>
+      <div className="carouselImg">
         <Carousel>
           <Carousel.Item>
             <img
-              className="imgCarrusel d-block w-100"
+              className="imgCarrusel d-block w-60"
               src={images(`./img1.jpg`)}
               alt="s"
             />
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="imgCarrusel d-block w-100"
+              className="imgCarrusel d-block w-60"
               src={images(`./img2.jpg`)}
               alt="s"
             />
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="imgCarrusel d-block w-100"
+              className="imgCarrusel d-block w-60"
               src={images(`./img3.jpg`)}
               alt="s"
             />
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="imgCarrusel d-block w-100"
+              className="imgCarrusel d-block w-60"
               src={images(`./img4.jpg`)}
               alt="s"
             />
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="imgCarrusel d-block w-100"
+              className="imgCarrusel d-block w-60"
               src={images(`./img5.jpg`)}
               alt="s"
             />
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="imgCarrusel d-block w-100"
+              className="imgCarrusel d-block w-60"
               src={images(`./img6.jpg`)}
               alt="s"
             />
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="imgCarrusel d-block w-100"
+              className="imgCarrusel d-block w-60"
               src={images(`./img7.jpg`)}
               alt="s"
             />
           </Carousel.Item>
         </Carousel>
       </div>
-      <div>
-        <img src={logo} alt="s" />
+      <div className="mainContainer">
+        <div className="logo">
+          <img src={logo} alt="s" />
+        </div>
+        <div className="litleTitle">
+          <h6>HACEMOS POSIBLE LA SOLUCIÓN DE TU PROBLEMA</h6>
+          <h1>
+            Encuentra la persona ideal <br /> para tu necesidad actual
+          </h1>
+        </div>
       </div>
-      <div>frase de la compania</div>
+
       <div>imagen backgraund</div>
       <div>dos botones: uno que redireccione a los contratistas </div>
     </>
